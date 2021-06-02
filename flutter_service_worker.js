557ef855935043eb6e27dddd10f1b36b0eaa71f8
mosh <mosh.onaarajnr@gmail.com>
2021-06-02 10:09:10 +0100

    initial commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "190e59b58292a5aadc6cfaabe9337618",
-"index.html": "0ac885604e7d7eceae165d2399f00e32",
-"/": "0ac885604e7d7eceae165d2399f00e32",
-"main.dart.js": "4a2f2b43153c521052d1d28d3b909cdd",
+"index.html": "01cb0ac9f169c3e537dc17e278553466",
+"/": "01cb0ac9f169c3e537dc17e278553466",
+"main.dart.js": "26fe70b721d3d67c571c2613d96bcba2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
